062e9177ea4d54adf588196fea25c664c875b33c
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-08-19 14:26:52 +0500

    Update index.js
    Fixed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import reactDom from 'react-dom';
 import GlobalState from './context/global/GlobalState';
 import Main from './Main';
